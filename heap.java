a4d5e3a711c41bf82df0b556a9e4e0f7727bef40
evanger123 <evenger125@gmail.com>
2022-07-07 13:24:21 -0400

    i dont feel like dheap

---
+++ heap.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class heap {
+    private ArrayList<Integer> theHeap; 
+    public heap(){
+        this.theHeap = new ArrayList<Integer>(); 
+    }
+    public void add(int value){
+        theHeap.add(value);
+    }
+    public void sort(){
+        
+    }
+
+    public static void main(String[] args){
+
+    }
+}
