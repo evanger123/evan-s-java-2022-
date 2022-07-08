06b1331c993d25319d9957b3d35d3d9cb5544625
evanger123 <evenger125@gmail.com>
2022-07-08 12:08:08 -0400

    wow i love this program!

--- FunnyProgram.java
+++ FunnyProgram.java
@@ -7,25 +7,35 @@ class Yarn implements Runnable{
     private static int loc = 0;
     private static int loc2 = 0;
     private static boolean upDown = true;
+    private static boolean leftRight = true;
     public void run(){
         
         if (upDown == true){
-            loc+=10;
             loc2+=10;
-            if(loc2==600){
+            if(loc2==800){
                 upDown=false;
             }
         }
-        if (upDown == false){
+        if (leftRight==true){
             loc+=10;
+            if (loc==1400){
+                leftRight=false;
+            }
+        }
+        if (upDown == false){   
             loc2-=10;
             if(loc2==0){
                 upDown=true;
             }
         }
-        
+        if (leftRight==false){
+            loc-=10;
+            if (loc==0){
+                leftRight=true;
+            }
+        }        
         JFrame frame = new JFrame();
-        frame.setSize(600,600);
+        frame.setSize(300,300);
         // frame.setLocation(rand.nextInt(1900), rand.nextInt(1900));
         frame.setLocation(loc, loc2);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
